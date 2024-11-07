efebbc90126bf9142c1017c9a7490182d18f3739
Alberto Cazallas Monje <115149185+AlbertoCazallasMonje@users.noreply.github.com>
2024-11-07 09:41:52 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,12 @@
 # Espera unos segundos para asegurar que el servidor esté listo
 sleep 15
 
-# Ejecuta el script de inicialización
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -d master -i /usr/src/app/init-db.sql
+# Ejecuta el script de inicialización si `sqlcmd` está disponible
+if [ -f "/opt/mssql-tools/bin/sqlcmd" ]; then
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -d master -i /usr/src/app/init-db.sql
+else
+    echo "sqlcmd no está disponible en /opt/mssql-tools/bin/sqlcmd"
+fi
 
 # Espera indefinidamente para mantener el contenedor activo
 wait
