6bb4145b473b04ac174d37294e779a23cc18322c
Alberto Cazallas Monje <115149185+AlbertoCazallasMonje@users.noreply.github.com>
2024-11-07 10:22:21 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,15 @@
 # Inicia SQL Server en segundo plano
 /opt/mssql/bin/sqlservr &
 
-# Espera unos segundos para asegurar que el servidor esté listo
+# Espera hasta que SQL Server esté completamente inicializado
 sleep 15
 
-# Ejecuta el script de inicialización si `sqlcmd` está disponible
-if [ -f "/opt/mssql-tools/bin/sqlcmd" ]; then
+# Verifica si sqlcmd está disponible y ejecuta el script de inicialización
+if command -v sqlcmd > /dev/null; then
     /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -d master -i /usr/src/app/init-db.sql
 else
     echo "sqlcmd no está disponible en /opt/mssql-tools/bin/sqlcmd"
 fi
 
-# Espera indefinidamente para mantener el contenedor activo
+# Mantiene el contenedor activo
 wait
